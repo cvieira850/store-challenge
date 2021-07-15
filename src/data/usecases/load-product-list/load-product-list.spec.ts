21dbcd1d65599b4db3eb9027466bc60d51c4bb55
Caio Vieira <contato@caiovieira.com.br>
2021-07-14 21:45:59 -0300

    refactor: move HttpGetClientSpy to its own file

--- src/data/usecases/load-product-list/load-product-list.spec.ts
+++ src/data/usecases/load-product-list/load-product-list.spec.ts
@@ -1,15 +1,8 @@
 import { LoadProductList } from './load-product-list'
-import { HttpGetClient } from '../../protocols/http/http-get-client'
+import { HttpGetClientSpy } from '../../test/mock-http-client'
 
 describe('LoadProductList', () => {
   test('Should call HttpClient with correct URL', async () => {
-    class HttpGetClientSpy implements HttpGetClient {
-      url?: string
-      async get (url: string): Promise<void> {
-        this.url = url
-        return Promise.resolve()
-      }
-    }
     const url = 'any_url'
     const httpGetClientSpy = new HttpGetClientSpy()
     const sut = new LoadProductList(url, httpGetClientSpy)
