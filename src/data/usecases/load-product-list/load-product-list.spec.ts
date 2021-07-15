3e18f24cef7797d5084cdd5237af44c7140b6f57
Caio Vieira <contato@caiovieira.com.br>
2021-07-14 22:08:11 -0300

    refactor: move sut creation to a factory helper method

--- src/data/usecases/load-product-list/load-product-list.spec.ts
+++ src/data/usecases/load-product-list/load-product-list.spec.ts
@@ -1,11 +1,24 @@
 import { LoadProductList } from './load-product-list'
 import { HttpGetClientSpy } from '../../test/mock-http-client'
 
+type SutTypes = {
+  sut: LoadProductList
+  httpGetClientSpy: HttpGetClientSpy
+}
+
+const makeSut = (url: string = 'any_url'): SutTypes => {
+  const httpGetClientSpy = new HttpGetClientSpy()
+  const sut = new LoadProductList(url, httpGetClientSpy)
+  return {
+    sut,
+    httpGetClientSpy
+  }
+}
+
 describe('LoadProductList', () => {
   test('Should call HttpClient with correct URL', async () => {
-    const url = 'any_url'
-    const httpGetClientSpy = new HttpGetClientSpy()
-    const sut = new LoadProductList(url, httpGetClientSpy)
+    const url = 'other_url'
+    const { sut, httpGetClientSpy } = makeSut(url)
     await sut.loadAll()
     expect(httpGetClientSpy.url).toBe(url)
   })
