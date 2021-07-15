c09f45abfd080682d7909df19d24af07ca715f7d
Caio Vieira <contato@caiovieira.com.br>
2021-07-15 18:43:21 -0300

    refactor: change LoadProductList to use faker

--- src/data/usecases/load-product-list/load-product-list.spec.ts
+++ src/data/usecases/load-product-list/load-product-list.spec.ts
@@ -1,12 +1,13 @@
 import { LoadProductList } from './load-product-list'
 import { HttpGetClientSpy } from '../../test/mock-http-client'
+import faker from 'faker'
 
 type SutTypes = {
   sut: LoadProductList
   httpGetClientSpy: HttpGetClientSpy
 }
 
-const makeSut = (url: string = 'any_url'): SutTypes => {
+const makeSut = (url: string = faker.internet.url()): SutTypes => {
   const httpGetClientSpy = new HttpGetClientSpy()
   const sut = new LoadProductList(url, httpGetClientSpy)
   return {
@@ -17,7 +18,7 @@ const makeSut = (url: string = 'any_url'): SutTypes => {
 
 describe('LoadProductList', () => {
   test('Should call HttpClient with correct URL', async () => {
-    const url = 'other_url'
+    const url = faker.internet.url()
     const { sut, httpGetClientSpy } = makeSut(url)
     await sut.loadAll()
     expect(httpGetClientSpy.url).toBe(url)
