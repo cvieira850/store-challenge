ffb6ded8b3d5967dd2802b418e98cac3bb964ce5
Caio Vieira <contato@caiovieira.com.br>
2021-07-17 15:19:37 -0300

    feat: ensure AxiosHttpClient return the correct statusCode and body

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,7 +1,13 @@
+import { HttpGetClient, HttpResponse } from '@/data/protocols/http'
 import axios from 'axios'
 
-export class AxiosHttpClient {
-  async get (url: string): Promise<void> {
-    await axios.get(url)
+export class AxiosHttpClient implements HttpGetClient<any> {
+  async get (url: string): Promise<HttpResponse<any>> {
+    const httpResponse = await axios.get(url)
+
+    return {
+      statusCode: httpResponse.status,
+      body: httpResponse.data
+    }
   }
 }
