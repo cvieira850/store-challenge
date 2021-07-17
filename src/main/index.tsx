e94d20fbc727adf1cddf368055fd9f9a74d03409
Caio Vieira <contato@caiovieira.com.br>
2021-07-17 20:04:46 -0300

    feat: add loadProducts view

---
+++ src/main/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ListProducts from '@/presentation/pages/list-products/list-products'
+
+ReactDOM.render(
+  <ListProducts />,
+  document.getElementById('main')
+)
---
+++ src/presentation/pages/list-products/list-products.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Styles from './list-products-styles.scss'
+
+const ListProducts: React.FC = () => {
+  return (
+    <ul className={Styles.productlist}>
+      <li >
+        <img src={'https://picsum.photos/200/300'} alt={'llll'} />
+        <strong >teste</strong>
+        <span >descricao</span>
+        <button
+          type="button"
+          data-testid="add-product-button"
+
+          onClick={() =>
+            console.log('lalala')
+          }
+        >
+          <div data-testid="cart-product-quantity">
+            0
+          </div>
+
+          <span >ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ul>
+  )
+}
+
+export default ListProducts
